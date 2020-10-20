6beadbd585696c034fedb2ed0661545b22286a84
Zhana Duren <36247822+durenzn@users.noreply.github.com>
2020-10-20 11:56:35 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,4 @@
 #/bin/bash
 
 #download the prior data 
-wget https://drive.google.com/file/d/15uTdAqHsLanaUZGFgLqTiLRXjv8DDZJ6/view?usp=sharing
-unzip prior.zip
-rm prior.zip
-wget https://drive.google.com/file/d/1FOY17wjUV_RV5OMaxCn6mudhjyZVvlE7/view?usp=sharing
-unzip example.zip
-rm example.zip
-mkdir ./Results/
-mkdir ./Homer/
+gunzip ./prior/RE_gene_corr*
